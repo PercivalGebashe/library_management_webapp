0839ca688336a0bbcfed08300f6cfd3a21e7a9d0
Percival Gebashe <percivalgebashe@gmail.com>
2025-02-12 18:17:48 +0200

    Moved server side logic to server side project

--- src/main/java/com/github/percivalgebashe/assignment_5/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.percivalgebashe.assignment_5.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
